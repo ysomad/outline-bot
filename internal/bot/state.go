bf2ae595747dc3ccbbbad2f28144dee2a3fd8079
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2024-06-23 02:08:50 +0700

    chore: move user to its own file

--- internal/bot/state.go
+++ internal/bot/state.go
@@ -1,13 +1,5 @@
 package bot
 
-import (
-	"fmt"
-	"io"
-	"strconv"
-
-	"gopkg.in/telebot.v3"
-)
-
 type step string
 
 const (
@@ -28,49 +20,4 @@ type State struct {
 type order struct {
 	keyAmount int8
 	sum       int16
-}
-
-type user struct {
-	id        int64
-	username  string
-	firstName string
-	lastName  string
-}
-
-func recipient(uid int64) user {
-	return user{id: uid}
-}
-
-func newUser(c *telebot.Chat) user {
-	return user{
-		id:        c.ID,
-		username:  c.Username,
-		firstName: c.FirstName,
-		lastName:  c.LastName,
-	}
-}
-
-func (u user) ID() string {
-	return strconv.FormatInt(u.id, 10)
-}
-
-// Recipient implements telebot.Recipient
-func (u user) Recipient() string {
-	if u.username != "" {
-		return u.username
-	}
-	return u.ID()
-}
-
-func (u user) Write(w io.Writer) {
-	fmt.Fprintf(w, "ID: %d", u.id)
-	if u.username != "" {
-		fmt.Fprintf(w, "\nЛогин: @%s", u.username)
-	}
-	if u.firstName != "" {
-		fmt.Fprintf(w, "\nИмя: %s", u.firstName)
-	}
-	if u.lastName != "" {
-		fmt.Fprintf(w, "\nФамилия: %s", u.lastName)
-	}
 }
